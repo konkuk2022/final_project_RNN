4df166baf009f07beb06e531c08b4cd844d882cf
창해 <changhea08@naver.com>
2022-08-25 10:32:52 +0900

    mac_version

--- data_processing.py
+++ data_processing.py
@@ -7,9 +7,6 @@ import kss
 
 import pandas as pd
 
-import os
-os.environ["TOKENIZERS_PARALLELISM"] = "false"
-
 def one_hot_encode(data, n_label=44):
     data = list(map(int,data.split(',')))
     one_hot = [0] * n_label
@@ -38,56 +35,12 @@ def data_preprocessing(df,electra_model):
     return pr_x, pr_y
 
 
-LABELS = ['불평/불만',
- '환영/호의',
- '감동/감탄',
- '지긋지긋',
- '고마움',
- '슬픔',
- '화남/분노',
- '존경',
- '기대감',
- '우쭐댐/무시함',
- '안타까움/실망',
- '비장함',
- '의심/불신',
- '뿌듯함',
- '편안/쾌적',
- '신기함/관심',
- '아껴주는',
- '부끄러움',
- '공포/무서움',
- '절망',
- '한심함',
- '역겨움/징그러움',
- '짜증',
- '어이없음',
- '없음',
- '패배/자기혐오',
- '귀찮음',
- '힘듦/지침',
- '즐거움/신남',
- '깨달음',
- '죄책감',
- '증오/혐오',
- '흐뭇함(귀여움/예쁨)',
- '당황/난처',
- '경악',
- '부담/안_내킴',
- '서러움',
- '재미없음',
- '불쌍함/연민',
- '놀람',
- '행복',
- '불안/걱정',
- '기쁨',
- '안심/신뢰']
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
+device = "mps" if torch.backends.mps.is_available() else "cpu"
 
 class KOTEtagger(pl.LightningModule):
     def __init__(self):
         super().__init__()
+
         self.electra = ElectraModel.from_pretrained("beomi/KcELECTRA-base").to(device)
         self.tokenizer = AutoTokenizer.from_pretrained("beomi/KcELECTRA-base", TOKENIZERS_PARALLELISM=True)
         self.classifier = nn.Linear(self.electra.config.hidden_size, 44).to(device)
