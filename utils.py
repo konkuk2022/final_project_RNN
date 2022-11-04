11bf14ff9c19b92630208341d9ef8057dd6b7ce7
창해 <97149652+jungchanghae@users.noreply.github.com>
2022-11-04 18:05:45 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,22 @@
-# cosine similarity
+import kss
 
+# cosine similarity
 def cos_similiarity(v1, v2):
     dot_product = np.dot(v1, v2)
     l2_norm = (np.sqrt(sum(np.square(v1)))*np.sqrt(sum(np.square(v2))))
     similarity = dot_product/l2_norm
 
     return similarity
+
+
+
+# 여러 문장을 나눠서 sep 토큰을 대입
+def kss_sentence(sent):
+    x = ''
+    split_sent = kss.split_sentences(sent)
+    for i,s in enumerate(split_sent):
+        if i == 0:
+            x = s
+        else:
+            x += ' [SEP] ' + s
+    return x
